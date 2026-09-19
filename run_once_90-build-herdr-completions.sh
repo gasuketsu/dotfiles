047ab75f0e513debb27a98cc4c0db248da89b69d
HARADA Tomoyuki <harada.tomoyuki03@gmail.com>
2026-09-19 20:31:32 +0900

    chore: build herdr completions with devbox global shellenv

--- run_once_90-build-herdr-completions.sh
+++ run_once_90-build-herdr-completions.sh
@@ -1,11 +1,6 @@
 #! /usr/bin/env bash
 
-if [ -z "${__ETC_PROFILE_NIX_SOURCED}" ]; then
-    . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
-fi
-
-if ! command -v herdr >/dev/null 2>&1; then
-    exit 0
-fi
+# make sure devbox global is activated
+eval "$(devbox global shellenv)"
 
 herdr completion zsh >~/.config/zsh/completions/_herdr
